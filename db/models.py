4c62a68fb74e481611fb086b0e074edda14f22cd
lubkoserhii <lubko.serhii@gmail.com.com>
2026-05-05 08:38:53 +0300

    Solution2 with no changes

--- db/models.py
+++ db/models.py
@@ -75,7 +75,7 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return f"<Order: {self.created_at}>"
+        return str(self.created_at)
 
 
 class Ticket(models.Model):
@@ -102,9 +102,9 @@ class Ticket(models.Model):
 
     def __str__(self) -> str:
         return (
-            f"<Ticket: {self.movie_session.movie.title} "
+            f"{self.movie_session.movie.title} "
             f"{self.movie_session.show_time} "
-            f"(row: {self.row}, seat: {self.seat})>"
+            f"(row: {self.row}, seat: {self.seat})"
         )
 
     def clean(self) -> None:
